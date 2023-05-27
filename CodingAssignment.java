636315d413e3cff1a9d867aec2aefa9733498d2d
nsperera1 <128836792+nsperera1@users.noreply.github.com>
2023-05-27 15:56:41 -0600

    Add files via upload

---
+++ CodingAssignment.java
@@ -0,0 +1,72 @@
+package week4;
+
+public class CodingAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
+	System.out.println(3-93);
+	
+	int[] newAges = {3, 9, 23, 64, 2, 8, 28, 93, 95};
+	System.out.println(3-95);
+	}
+	
+	int total = 0;
+	for (int e: num) {
+		total = total + e;
+	System.out.println("avg is: " + total/newAges.length);
+	}
+	
+	String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+	for (int counter = 0; counter < names.length; names++) {
+		System.out.println(names[counter] + " ");
+		System.out.println("Sam" + " " + "Tommy" + " " + "Tim" + " " + "Sally" + " " + "Buck" + " " + "Bob");
+	}
+	
+	int[] nameLengths = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+	for (nameLengths = 0; nameLengths < names; nameLengths++) {
+		System.out.println(nameLengths.length);
+	
+	System.out.println(multiplyString("Hello", 3));
+	}
+	
+	
+	
+	
+
+
+public static String multiplyString(String str, int num) {
+	String result = "";
+	for (int i = 0; i < num; i++ ) {
+		result += str
+	}
+	return result;
+}
+
+
+public static String createFullName(String x, String y) {
+	String fullName = x + " " + y
+	return fullName;
+}
+
+public static double calculateAverage(double[] array) {
+	double sum = 0;
+	for (double number : array) {
+		sum += number;
+	}
+	return sum / array.length;
+
+
+public static String createStudentList(String x, String y, String z) {
+	String studentList = x + " " + y + " " + z
+	return studentList
+			
+	//I created this method for a student roster and it includes 3 students (for a very small class) with their names and a space in between.
+}
+
+}
+
+	
+	
+
